c0cc3fbeb5492b61af784eb03a4b695021af23e0
Malcome Fein <mfein90@gmail.com>
2018-11-27 18:13:46 -0500

    updated story.rb

--- lib/techcrunch_cli/story.rb
+++ lib/techcrunch_cli/story.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class TechcrunchCli::Story
   # We never instantiate instances of Stories.
   # Rather, we just call class methods on this all time.
@@ -7,21 +8,23 @@ class TechcrunchCli::Story
 
   def self.scrape_content(url)
     @doc = Nokogiri::HTML(open(url))
+    #binding.pry
     content = @doc.search("div.article-entry").text.strip
   end
 
   def self.scrape_urls
-    @doc = Nokogiri::HTML(open('http://techcrunch.com/'))
+    @doc = Nokogiri::HTML(open('https://techcrunch.com/'))
 
     hrefs = [] # always the same
     @doc.search("ul#river1 h2.post-title a").each do |a|
       hrefs << a.attr("href") # different
+
     end
     hrefs # always the same
   end
 
   def self.scrape_titles
-    @doc = Nokogiri::HTML(open('http://techcrunch.com/'))
+    @doc = Nokogiri::HTML(open('https://techcrunch.com/'))
 
     titles = [] # always the same
     @doc.search("ul#river1 h2.post-title").each do |h2|
@@ -31,14 +34,15 @@ class TechcrunchCli::Story
   end
 
   def self.scrape_authors
-    @doc = Nokogiri::HTML(open('http://techcrunch.com/'))
+    @doc = Nokogiri::HTML(open('https://techcrunch.com/'))
     authors = @doc.search("ul#river1 div.byline").text.split("by")
     authors
   end
 
   def self.scrape_summaries
-    @doc = Nokogiri::HTML(open('http://techcrunch.com/'))
+    @doc = Nokogiri::HTML(open('https://techcrunch.com/'))
     summaries = @doc.search("ul#river1 p.excerpt").text
     summaries.split("Read More")
   end
 end
+TechcrunchCli::Story.scrape_content('https://techcrunch.com/')
