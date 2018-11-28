420b71d7b6444a369db9b32cc26a8f3ace9c5de2
Malcome Fein <mfein90@gmail.com>
2018-11-28 18:46:46 -0500

    Done.

--- lib/techcrunch_cli/story.rb
+++ lib/techcrunch_cli/story.rb
@@ -1,7 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-class Story
+ #class Story
+ class TechcrunchCli::Story
   # We never instantiate instances of Stories.
   # Rather, we just call class methods on this all time.
 
@@ -11,7 +12,7 @@ class Story
   def self.scrape_content(url)
     @doc = Nokogiri::HTML(open(url))
     # binding.pry
-    content = @doc.search("div.article-entry").text.strip
+    content = @doc.search("div.post-block post-block--image post-block--unread").text.strip
   end
 
   def self.scrape_urls
@@ -25,20 +26,21 @@ class Story
     hrefs # always the same
   end
 
-  def self.scrape_titles
+  def self.scrape_titles #Done.
     @doc = Nokogiri::HTML(open('https://techcrunch.com/'))
-
+    #binding.pry
     titles = [] # always the same
-    @doc.search("ul#river1 h2.post-title").each do |h2|
-      titles << h2.text # different
+    @doc.search("h2.post-block__title").each do |h2|
+      titles << h2.text.strip # different
     end
     titles # always the same
   end
 
   def self.scrape_authors
     @doc = Nokogiri::HTML(open('https://techcrunch.com/'))
-    binding.pry
-    authors = @doc.search("ul#river1 div.byline").text.split("by")
+    #binding.pry
+    # authors = @doc.search("span.river-byline__authors a").text.split("by")
+    authors = @doc.search("span.river-byline__authors a").first.text.strip.split("by")
     authors
   end
 
@@ -49,7 +51,8 @@ class Story
   end
   # self.scrape_content('https://techcrunch.com/')
   # self.scrape_urls
-  self.scrape_authors
+  # self.scrape_authors
+  #self.scrape_titles
 end
 #It's really helpful to have access to pry for nokogiri!
 # I also recommend taking a close look at the html of the site you're scraping in the developer console
